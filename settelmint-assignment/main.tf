104432ee6aa50c1a4eb1c14668557008645df37e
mgathe <maheshgathe@gmail.com>
2023-08-31 14:35:18 +0530

    settlemint assignment

---
+++ settelmint-assignment/main.tf
@@ -0,0 +1,18 @@
+#settup terraform state storage within azure. 
+
+terraform {
+    backend "azurerm"{
+        storage account name = "assignment1312"
+        container_name = "settelmint-assignment"
+        key            = "tfstate"
+        resource_group_name= "TFSTATE"
+        subscription_id = "a0bb738b-7d5a-48ae-87db-4aa0c520444b"
+    }
+}
+
+# resource group to contain all the resources of this assignment
+resource "azurerm_resource_group" "example" {
+  name     = "settelmint"
+  location = var.azure_region
+  tags     = var.resource_tags
+}
