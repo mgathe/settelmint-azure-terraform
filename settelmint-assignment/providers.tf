a62a91229ae53f4201f5c79a97300898a38e23d3
mgathe <maheshgathe@gmail.com>
2023-08-31 15:28:09 +0530

    adding workflow action

--- settelmint-assignment/providers.tf
+++ settelmint-assignment/providers.tf
@@ -2,6 +2,8 @@ provider "azurerm" {
   version         = "~>3.56.0"
   subscription_id = "a0bb738b-7d5a-48ae-87db-4aa0c520444b"
   #This is free trial subscription.I am using this only for creating resources for assignment
+  skip_provider_registration = true
+  features {}
 }
 
 provider "azuread" {
